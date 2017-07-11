68f8d7bd2a96843a31d440cd120bb269043dd1e1
salah <salah@linkry.events>
2017-07-11 16:55:54 +0100

    adding the draw line function

--- js/pie.js
+++ js/pie.js
@@ -3,3 +3,20 @@ myCanvas.width = 300;
 myCanvas.height = 300;
 
 var ctx = myCanvas.getContext("2d");
+
+/** draw line function **/
+
+function drawLine(ctx, startX, startY, endX, endY){
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+}
+
+/*
+ctx: reference to the drawing context
+startX: the X coordinate of the line starting point
+startY: the Y coordinate of the line starting point
+endX: the X coordinate of the line end point
+endY: the Y coordinate of the line end point
+*/
